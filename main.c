b68bcadeceb41caf98bf3a6487d9fff3df468dcc
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2022-05-31 21:56:09 +0900

    add print debug

--- main.c
+++ main.c
@@ -13,9 +13,13 @@ int main(void)
     int a[5] = {1, 2, 3, 4, 5};
     int b, c;
 
+    printf("c=%d, a1=%d, a5=%d\n", c, a[1], a[5]);
     c = a[1] + a[5];
+    printf("c=%d, a1=%d, a5=%d\n", c, a[1], a[5]);
 
+    printf("b=%d, c=%d, a1=%d\n", b, c, a[1]);
     b = c + a[1];
+    printf("b=%d, c=%d, a1=%d\n", b, c, a[1]);
 
     b = myfunc(b, c);
 
